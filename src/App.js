f8ff054af6b4e5c1c8ee5ffb2b5a5c910ea7baaa
pgswinter <tuananh.nguyen.freelance@gmail.com>
2018-03-15 17:20:38 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hey! I am still stay on local! Push me up!
+      </div>
+    );
+  }
+}
+
+export default App;
